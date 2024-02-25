32d639d74e1d1658877aecd1a9e94d4ad2749edf
Pavindu Lakshan <pavindulakshan@gmail.com>
2024-02-25 19:41:33 +0530

    Add logic to hide version dropdown on unversioned routes

--- src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
+++ src/theme/NavbarItem/DocsVersionDropdownNavbarItem.js
@@ -8,13 +8,24 @@
 
 import React from 'react';
 import DocsVersionDropdownNavbarItem from '@theme-original/NavbarItem/DocsVersionDropdownNavbarItem';
-import {useActiveDocContext} from '@docusaurus/plugin-content-docs/client';
+import {useLocation} from '@docusaurus/router';
 
 export default function DocsVersionDropdownNavbarItemWrapper(props) {
   // (CUSTOM) Hide version dropdown on non-versioned pages
   // eslint-disable-next-line react/destructuring-assignment
-  const activeDocContext = useActiveDocContext(props.docsPluginId);
-  if (!activeDocContext.activeDoc) {
+  // const activeDocContext = useActiveDocContext(props.docsPluginId);
+  const location = useLocation();
+
+  const unversionedRoutes = [
+    /^\/blog(?:\/[\w-]+)?(?:\/#\w+)?$/g
+  ]
+
+  function checkPathname(pathname) {
+    // Check if the provided pathname matches any of the regexes in the list
+    return unversionedRoutes.some(regex => regex.test(pathname))
+  }
+
+  if (checkPathname(location.pathname)) {
     return null;
   }
 
