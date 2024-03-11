6f4a7a1ec3f58c5e5c4fc6b881131338193dd871
Pavindu Lakshan <pavindulakshan@gmail.com>
2024-03-11 08:08:47 +0530

    Set algolia contexual search false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,7 +110,7 @@ const config = {
         indexName: 'wso2identityserver',
   
         // Optional: see doc section below
-        contextualSearch: true,
+        contextualSearch: false,
   
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: 'external\\.com|domain\\.com',
